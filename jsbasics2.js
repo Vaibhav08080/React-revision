8ddedc3c5aeca7a0b8a152d7672dd343269d6372
Vaibhav08080 <vibhuuu05@gmail.com>
2026-07-11 20:29:51 +0530

    2comit

---
+++ jsbasics2.js
@@ -0,0 +1,68 @@
+arr2=[2,3,4]
+function getinfo(a,b,...c){
+    console.log(a,b,c)
+}
+console.log(getinfo(1,2,3,4,4,5,5))
+
+
+//map , filter , find ,some , every , includes , indexof , findindex
+
+
+const personArryay=[
+    {
+        name:"vaibhav",
+        age:30,
+        country:"America"
+
+    },
+     {
+        name:"king",
+        age:30,
+        country:"America"
+
+    },
+     {
+        name:"meow",
+        age:30,
+        country:"America"
+
+    }
+]
+
+
+let getallnames= personArryay.map((singleperson, index)=>{
+    return `${singleperson.name} age is ${singleperson.age}`
+
+})
+
+console.log(getallnames)
+
+
+
+let personfromusa = personArryay.find((singleperson,idx)=>{
+    return singleperson
+    .country==="America"
+})
+console.log(personfromusa)
+
+
+let personfromusa2 = personArryay.filter((singleperson,idx)=>{
+    return singleperson
+    .country==="America"
+})
+console.log(personfromusa2)
+
+
+
+let checksomearrayexample=personArryay.some((simgleperson)=>{
+    return simgleperson.age>30
+
+})
+
+console.log(checksomearrayexample)
+
+
+let checkeveryexample=personArryay.every((p)=>{
+    return p.age<100
+})
+console.log(checkeveryexample)
--- react.js
+++ react.js
@@ -1,1 +1,92 @@
-console.log("Hi this is the react revision")
+console.log("Hi this is the react revision");
+
+let a = false;
+let b = true;
+
+console.log(a && b);
+
+// it will first check only the first value if its true
+// then only it will check the other one is true or not
+
+function getName(name) {
+  return name;
+}
+
+console.log(a && getName("meow"));
+
+//the first value is false thereofre it is also returning th e
+// false
+
+let c = false;
+let d = true;
+//in scenario of or it will check the second value too
+
+// template literals
+
+let name1 = "vaibhav";
+let name2 = "john";
+console.log(name1 + " " + name2, `${name1} ${name2}`);
+
+// ternary operator
+
+let showRecipeOne = false;
+function getRecipieName(re) {
+  return re;
+}
+
+function getRecipieName2(re) {
+  return re;
+}
+
+if (showRecipeOne) {
+  console.log(getRecipieName("meow"));
+} else {
+  console.log(getRecipieName2("ladle ghop ghop"));
+}
+
+// condition ? statement 1 : statement 2
+
+const id = 1;
+const productName = "Apple watch";
+const rating = 5;
+
+const product = {
+  id,
+  productName,
+  rating,
+};
+
+const product2 = {
+  description: "meowww",
+  id,
+  productName,
+  rating,
+};
+//destructuring the array and object
+
+const { description } = product2;
+console.log(description);
+
+const array = [1, 2, 3];
+
+console.log(...array)
+
+const [arrayfirstname, arraysecondname] = array;
+
+console.log(arrayfirstname, arraysecondname);
+
+
+
+
+
+
+
+
+
+function multiplicationoftwonumbers(num1=1,num2=4){
+    return num1*num2
+}
+
+console.log(multiplicationoftwonumbers(1,2))
+
+
